bf19c962a43982a8dffb108ca31e95b5662879b9
Ofer Buskila <ofer@pointer.co.il>
2021-10-31 15:28:02 +0200

    update ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,72 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
+// import {Component, HostListener, OnInit} from '@angular/core';
+// import {HttpClient, HttpParams} from "@angular/common/http";
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements  OnInit {
+//   title = 'antiPhishing';
+//   public innerWidth: any;
+//   public innerHeight: any;
+//   public result : string | undefined;
+
+//   constructor(private  http: HttpClient) {
+
+//   }
+//   @HostListener('window:resize', ['$event'])
+//   onResize() {
+//     this.innerWidth = window.innerWidth;
+//     this.innerHeight = window.innerHeight
+//   }
+//   ngOnInit(): void {
+//     this.innerWidth = window.innerWidth;
+//     this.innerHeight = window.innerHeight
+//   }
+
+
+//   isPhishing(){
+//     // let params = {"body": 'sdfsdf'}
+//     let params = new HttpParams();
+//     params = params.append('url', 'sfd');
+
+//     this.http.get('https://flask-anti-phishing.herokuapp.com/analyzeMessageFromAPI/?msg=', {params :params}).subscribe(response=>{
+//       // let re = JSON.stringify( response);
+//       var jsonResponse = JSON.parse(JSON.stringify(response));
+//       console.log(jsonResponse)
+//        let percentage= jsonResponse['percentage'];
+//        let recommended = jsonResponse['additionalRecommendation'];
+//        let smiley = percentage > 50 ? ':(' : ':)'
+//       this.result ="The message is " +  percentage +"%" + ' phishing ' + smiley + '\n' + recommended;
+//     })
+
+//   }
+
+//   return() {
+//     this.result =undefined;
+//   }
+// }
+
+import { Component, HostListener, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Ng2LoadingSpinnerModule } from 'ng2-loading-spinner'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
+  showSpinner: any;
   title = 'antiPhishing';
   public innerWidth: any;
   public innerHeight: any;
-  public result : string | undefined;
+  public result: string | undefined;
+  public recommendedActions: string | undefined;
+  public msg = " ";
 
-  constructor(private  http: HttpClient) {
+  constructor(private http: HttpClient) {
 
   }
   @HostListener('window:resize', ['$event'])
@@ -26,25 +80,32 @@ export class AppComponent implements  OnInit {
   }
 
 
-  isPhishing(){
+  isPhishing() {
     // let params = {"body": 'sdfsdf'}
+    this.showSpinner = true;
     let params = new HttpParams();
-    params = params.append('url', 'sfd');
+    params = params.append('msg', this.msg);
 
-    this.http.get('https://flask-anti-phishing.herokuapp.com/analyzeMessageFromAPI/?msg=', {params :params}).subscribe(response=>{
+    this.http.get('http://127.0.0.1:5000/analyzeMessageFromAPI/', { params: params }).subscribe(response => {
       // let re = JSON.stringify( response);
+      this.showSpinner = false;
       var jsonResponse = JSON.parse(JSON.stringify(response));
-      console.log(jsonResponse)
-       let percentage= jsonResponse['percentage'];
-       let recommended = jsonResponse['additionalRecommendation'];
-       let smiley = percentage > 50 ? ':(' : ':)'
-      this.result ="The message is " +  percentage +"%" + ' phishing ' + smiley + '\n' + recommended;
+      let percentage = jsonResponse['percentage'];
+      this.recommendedActions = jsonResponse['additionalRecommendation'];
+      let smiley = percentage > 50 ? ':(' : ':)'
+      this.result = "The message is " + percentage + "%" + ' phishing ' + smiley;
     })
 
   }
 
   return() {
-    this.result =undefined;
+    this.result = undefined;
   }
+
+
+  // onEdit(event: Event) {
+  //   this.msg = event.target.value;
+  // }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+import { Ng2LoadingSpinnerModule } from "ng2-loading-spinner";
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    Ng2LoadingSpinnerModule.forRoot({ spinnerColor: 'black', spinnerSize: 'md', backdropColor: 'transparent' })
   ],
   providers: [],
   bootstrap: [AppComponent]
